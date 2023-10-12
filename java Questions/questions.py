1591025df5b638eb98407cdc8cde1fd58b3df57b
Aravindh9490 <haravind123143@gmail.com>
2023-10-12 20:36:59 +0530

    3 rd question

--- java Questions/questions.py
+++ java Questions/questions.py
@@ -15,7 +15,7 @@ def custom_shuffle(arr):
 custom_shuffle(original_array)
 
 
-print(original_array)#
+print(original_array)
 
 
 #2 Enter a Roman Number as input and convert it to an integer. (Example: IX = 9)
@@ -39,12 +39,25 @@ def roman_to_int(roman_numeral):
     return result
 
 
-roman_numeral = input("Enter a Roman numeral: ")
-
+roman_numeral = "XI"
 
 integer_value = roman_to_int(roman_numeral)
 print(integer_value)
 
+#3 Check if the input is pangram or not. (A pangram is a sentence that contains all the alphabets from A to Z)
+
+def is_pangram(input_string):
+    alphabet_set = set('abcdefghijklmnopqrstuvwxyz')
+    input_string = input_string.lower().replace(" ", "")
+    return set(alphabet_set).issubset(input_string)
+
+
+input_string = "The quick brown fox jumps over the lazy dog"
+
+if is_pangram(input_string):
+    print("The input is a pangram.")
+else:
+    print("The input is not a pangram.")
     
 
 
