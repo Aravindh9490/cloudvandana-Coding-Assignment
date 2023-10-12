b9a3c13d48a6cc1a15cbbc8f708b195023570560
Aravindh9490 <haravind123143@gmail.com>
2023-10-12 19:48:09 +0530

    first commit

---
+++ java Questions/questions.py
@@ -0,0 +1,12 @@
+# 1 Create an array with the values (1, 2, 3, 4, 5, 6, 7) and shuffle it.
+
+import random
+
+# Create the original array
+original_array = [1, 2, 3, 4, 5, 6, 7]
+
+# Shuffle the array in place
+random.shuffle(original_array)
+
+# The original_array is now shuffled
+print(original_array)
