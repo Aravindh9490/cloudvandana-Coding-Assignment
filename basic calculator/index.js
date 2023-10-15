edf5504716c7e3f064e4ab2ae67830516c411fa2
Aravindh9490 <haravind123143@gmail.com>
2023-10-15 14:13:48 +0530

    webpages

---
+++ basic calculator/index.js
@@ -0,0 +1,59 @@
+let display = document.getElementById("display");
+let currentOperand = "";
+let displayNum="";
+let firstOperand = null;
+
+let operator = null;
+
+function appendToDisplay(value) {
+  currentOperand += value;
+  displayNum=currentOperand
+  display.value = currentOperand;
+}
+
+function clearDisplay() {
+  currentOperand = '';
+  firstOperand = null;
+  
+  operator = null;
+  display.value = '';
+}
+
+function operate(op) {
+  if (firstOperand === null) {
+    firstOperand = parseFloat(currentOperand);
+    currentOperand = currentOperand + op;
+    operator = op;
+    display.value=currentOperand
+  } else if (currentOperand !== '') {
+    firstOperand = calculate();
+    currentOperand =currentOperand + op;
+    operator = op;
+    display.value=currentOperand
+  }
+}
+
+function calculate() {
+  // let secondOperand = parseFloat(currentOperand);
+  // let result;
+  // switch (operator) {
+  //   case '+':
+  //     result = firstOperand + secondOperand;
+  //     break;
+  //   case '-':
+  //     result = firstOperand - secondOperand;
+  //     break;
+  //   case '*':
+  //     result = firstOperand * secondOperand;
+  //     break;
+  //   case '/':
+  //     result = firstOperand / secondOperand;
+  //     break;
+  // }
+  result=eval(display.value)
+  display.value = result;
+  currentOperand = result.toString();
+  firstOperand = null;
+  operator = null;
+  return result;
+}
